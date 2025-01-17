9e42b913ed3b15b727a34dc92ab5b02e42443262
DhritiKohli <nsrivastava@stu.naperville203.org>
2025-01-16 20:03:46 -0600

    worked on autopickup.js

--- src/analysis/public/js/analysisPipeline.js
+++ src/analysis/public/js/analysisPipeline.js
@@ -1,3 +1,56 @@
+const fs = require("fs");
+const chalk = require("chalk");
+
+//enable debug logs
+const debug = false;
+
+/* load transformers */
+const transformers = {
+  tmp: {},
+  team: {},
+};
+
+for (let executableFile of fs.readdirSync(__dirname + "/transformers")) {
+  if (debug)
+    console.log(
+      `attempting to load transformer from ${
+        __dirname + "/transformers/" + executableFile
+      }`
+    );
+  let transformer = require("./transformers/" + executableFile);
+
+  if ("tmp" in transformer) {
+    //the transformer supports teamMatchPerformances
+    if (debug) console.log(`${transformer.tmp.name}.tmp - detected`);
+    if (transformer.tmp.name in transformers.tmp)
+      throw new Error(
+        chalk.whiteBright.bgRed.bold(
+          `${
+            transformers.tmp.name
+          }.tmp - transformer name duplicated! Transformer names must be unique.\nOther transformers loaded: [${Object.keys(
+            transformers.tmp
+          )}]`
+        )
+      );
+    transformers.tmp[transformer.tmp.name] = transformer.tmp;
+  }
+
+  if ("team" in transformer) {
+    //the transformer supports teams
+    if (debug) console.log(`${transformer.team.name}.team - detected`);
+    if (transformer.team.name in transformers.team)
+      throw new Error(
+        `${
+          transformers.team.name
+        } - transformer name duplicated! Transformer names must be unique.\nOther transformers loaded: [${Object.keys(
+          transformers.tmp
+        )}]`
+      );
+    transformers.team[transformer.team.name] = transformer.team;
+  }
+}
+if (debug) console.log("loaded all transformers!");
+
 async function executePipeline() {
   // Get tmps from database (or cache if offline)
   let tmps = await fetch("./api/dataset").then((res) => res.json());
@@ -56,64 +109,6 @@ async function executePipeline() {
 
   return dataset1;
 }
-
-const pipelineConfig = require("../../config/analysis-pipeline.json");
-const config = require("../../config/config.json");
-const fs = require("fs");
-const { TeamMatchPerformance } = require("../lib/db");
-const { Dataset } = require("./DataTransformer");
-const chalk = require("chalk");
-const { setPath } = require("../lib/util");
-
-//enable debug logs
-const debug = false;
-
-/* load transformers */
-const transformers = {
-  tmp: {},
-  team: {},
-};
-
-for (let executableFile of fs.readdirSync(__dirname + "/transformers")) {
-  if (debug)
-    console.log(
-      `attempting to load transformer from ${
-        __dirname + "/transformers/" + executableFile
-      }`
-    );
-  let transformer = require("./transformers/" + executableFile);
-
-  if ("tmp" in transformer) {
-    //the transformer supports teamMatchPerformances
-    if (debug) console.log(`${transformer.tmp.name}.tmp - detected`);
-    if (transformer.tmp.name in transformers.tmp)
-      throw new Error(
-        chalk.whiteBright.bgRed.bold(
-          `${
-            transformers.tmp.name
-          }.tmp - transformer name duplicated! Transformer names must be unique.\nOther transformers loaded: [${Object.keys(
-            transformers.tmp
-          )}]`
-        )
-      );
-    transformers.tmp[transformer.tmp.name] = transformer.tmp;
-  }
-
-  if ("team" in transformer) {
-    //the transformer supports teams
-    if (debug) console.log(`${transformer.team.name}.team - detected`);
-    if (transformer.team.name in transformers.team)
-      throw new Error(
-        `${
-          transformers.team.name
-        } - transformer name duplicated! Transformer names must be unique.\nOther transformers loaded: [${Object.keys(
-          transformers.tmp
-        )}]`
-      );
-    transformers.team[transformer.team.name] = transformer.team;
-  }
-}
-if (debug) console.log("loaded all transformers!");
 
 /* load manual data */
 const manual = {
